74f5c99fc066bd9e3f124c344e66d40165849437
walidmahade <walid_mahade@outlook.com>
2019-03-31 20:54:11 +0600

    added responsiveness

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,8 +1,11 @@
 jQuery(document).ready(function ($) {
+    /*
+        chat menu show hide toggle
+   */
    const chatWindow = $("#chat-window");
    const chatWindowToggle = $("#chat-window-toggle");
    // const chatList = $("#chat-list");
-   console.log(chatWindow.height());
+   // console.log(chatWindow.height());
    // intially keep chat window shinkred
    setTimeout(function () {
        chatWindow.css('bottom' , -chatWindow.height() + 60);
@@ -21,5 +24,43 @@ jQuery(document).ready(function ($) {
        toggleChatWindow();
    });
 
+   /*
+        Mobile navigation sticky trigger
+   */
+   const header = $(".header");
+
+   $(window).scroll(function () {
+       let sTop = $(window).scrollTop();
+       if (sTop >= 73) {
+           header.addClass('sticky--active');
+           $('body').addClass('fixed-header--active');
+       } else {
+           header.removeClass('sticky--active');
+           $('body').removeClass('fixed-header--active');
+       }
+   });
+
+   /*
+    // lightbox
+   */
+    const lightbox = $("#imageLightbox");
+    const lightboxOverlay = $("#lightboxOverlay");
+
+    function imageLightbox (img) {
+        lightboxOverlay.addClass('show-me');
+        lightbox.addClass('show-me');
+    }
+
+    $('.uf__post__image a').click(function (e) {
+        e.preventDefault();
+        let imgAddress = $(this).attr('href');
+        // console.log(imgAddress);
+        imageLightbox(imgAddress);
+    });
+
+    lightboxOverlay.click(function () {
+        lightbox.removeClass('show-me');
+        lightboxOverlay.removeClass('show-me');
+    });
 
 });
